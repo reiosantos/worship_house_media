ff2a7ad75fe46320d727091f8b7ad3bc50b1cbaa
santos <ronireiosantos@gmail.com>
2020-06-27 19:25:08 +0300

    - Add google maps key - Add Security configuration - Update gradle and compile SDK - Create VideoView component

--- lib/src/index.dart
+++ lib/src/index.dart
@@ -2,6 +2,7 @@ export 'dart:async';
 export 'dart:io';
 
 export 'package:bloc/bloc.dart';
+export 'package:chewie/chewie.dart';
 export 'package:flutter/cupertino.dart' hide RefreshCallback;
 export 'package:flutter/foundation.dart';
 export 'package:flutter/material.dart';
@@ -11,3 +12,5 @@ export 'package:http/http.dart';
 export 'package:meta/meta.dart';
 export 'package:rxdart/rxdart.dart' hide Notification;
 export 'package:shared_preferences/shared_preferences.dart';
+export 'package:video_player/video_player.dart';
+export 'package:wakelock/wakelock.dart';
--- lib/src/ui/screens/home.dart
+++ lib/src/ui/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:whm/src/index.dart';
+import 'package:whm/src/ui/widgets/video_view/video_view.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -8,15 +9,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<HomePage> {
+  List<String> videoUrls = <String>[
+    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+    'http://mirrors.standaloneinstaller.com/video-sample/dolbycanyon.3gp',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text('HOME PAGE'),
-        ],
+    return ListView.separated(
+      separatorBuilder: (BuildContext context, int index) => Divider(
+        height: 12.0,
+        color: Colors.blueGrey,
       ),
+      itemCount: videoUrls.length,
+      itemBuilder: (BuildContext context, int index) {
+        // TODO(reiosantos): Add description and title widget
+        return VideoView(videoUrls[index]);
+      },
     );
   }
 }
